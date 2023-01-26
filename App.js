b0e5fa388da3b71260f23c2270de94350b4ab41b
Pattaphon69 <pattaphon8390@gmail.com>
2023-01-26 23:06:20 +0700

    Revert "test"
    This reverts commit 4382acb0fbabc96fae6599cd352a81dde924f6bc.

--- App.js
+++ App.js
@@ -28,7 +28,7 @@ export default function App({ navigation }) {
       <Stack.Navigator
         initialRouteName='Login'
         screenOptions={{
-          headerShown: true
+          headerShown: false
         }}
       >
         <Stack.Screen name='Home' component={HomeScreen} />
